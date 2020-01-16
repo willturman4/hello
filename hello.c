afb02ad27810ae5a73e0df34a949ff60382d2a2a
willturman4 <willturman4@gmail.com>
2020-01-16 15:44:54 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+/* "Civil Engineering", My first program
+Name: William Turman
+Date: 2020/01/16
+*/
+#include <stdio.h>
+
+int main(void) {
+	 printf("///William Turman!\n");
+     printf("Civil Engineering!\n");
+     return0;
+}
